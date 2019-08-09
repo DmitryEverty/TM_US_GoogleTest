27dcf94d77d17a6d68f26fc8625a9b93b9a8a4f7
Dmitry Everty <evertymusic@gmail.com>
2019-08-09 17:00:36 +0300

    some mess

---
+++ CustomButton.js
@@ -0,0 +1,37 @@
+navigator.permissions.query({name: "clipboard-read"}).then(result => {
+    // If permission to read the clipboard is granted or if the user will
+    // be prompted to allow it, we proceed.
+  
+    if (result.state == "granted" || result.state == "prompt") {
+      navigator.clipboard.read().then(data => {
+        for (let i=0; i<data.items.length; i++) {
+          if (data.items[i].type != "text/plain") {
+            alert("Clipboard contains non-text data. Unable to access it.");
+          } else {
+            textElem.innerText = data.items[i].getAs("text/plain");
+          }
+        }
+      });
+    }
+  });
+
+var myNewButton = document.createElement("input"); 
+myNewButton.setAttribute("type", "submit")
+myNewButton.setAttribute("id", "myCustomBtn")
+myNewButton.setAttribute("value", '💖')
+// document.getElementsByClassName("FPdoLc")[0].getElementsByTagName("center")[0].appendChild(myNewButton)
+document.getElementsByClassName("FPdoLc")[0].getElementsByTagName("center")[0].insertBefore(myNewButton, document.getElementsByClassName("gNO89b")[1])
+
+document.getElementById("myCustomBtn").onclick = buttonText
+
+
+
+function buttonText(){ 
+    document.getElementsByClassName("gsfi")[1].value = ""   
+    navigator.clipboard.readText().then(function(value) {
+        document.getElementsByClassName("gsfi")[1].value = value
+        window.open('https://stackoverflow.com')
+        // document.getElementsByClassName('gNO89b')[1].click()
+    });
+}
+
