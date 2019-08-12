27f864d1fb36350b64d5c685dafb3d228d7c1b47
Dmitry Everty <evertymusic@gmail.com>
2019-08-12 18:57:56 +0300

    just something

--- CustomButton.js
+++ CustomButton.js
@@ -1,21 +1,21 @@
 navigator.permissions.query({name: "clipboard-read"}).then(result => {
     // If permission to read the clipboard is granted or if the user will
     // be prompted to allow it, we proceed.
-  
-    if (result.state == "granted" || result.state == "prompt") {
-      navigator.clipboard.read().then(data => {
-        for (let i=0; i<data.items.length; i++) {
-          if (data.items[i].type != "text/plain") {
-            alert("Clipboard contains non-text data. Unable to access it.");
-          } else {
-            textElem.innerText = data.items[i].getAs("text/plain");
-          }
-        }
-      });
-    }
-  });
 
-var myNewButton = document.createElement("input"); 
+    if (result.state == "granted" || result.state == "prompt") {
+        navigator.clipboard.read().then(data => {
+        for (let i=0; i<data.items.length; i++) {
+            if (data.items[i].type != "text/plain") {
+            alert("Clipboard contains non-text data. Unable to access it.");
+            } else {
+            textElem.innerText = data.items[i].getAs("text/plain");
+            }
+        }
+        });
+    }
+    });
+
+var myNewButton = document.createElement("input");
 myNewButton.setAttribute("type", "submit")
 myNewButton.setAttribute("id", "myCustomBtn")
 myNewButton.setAttribute("value", '💖')
@@ -30,8 +30,17 @@ function buttonText(){
     document.getElementsByClassName("gsfi")[1].value = ""   
     navigator.clipboard.readText().then(function(value) {
         document.getElementsByClassName("gsfi")[1].value = value
-        window.open('https://stackoverflow.com')
+        window.open('stackoverflow.com', '_top')
+        // window.location.replace("http://www.w3schools.com");
         // document.getElementsByClassName('gNO89b')[1].click()
     });
 }
 
+function copyText(){
+    let btnValue = document.getElementsByClassName("gNO89b")[1].getAttribute("value")
+    navigator.clipboard.writeText(btnValue).then(function(){
+        console.log(btnValue + 'was added to clipboard')
+    }, function(){
+        console.log('there was an error')
+    })
+}
